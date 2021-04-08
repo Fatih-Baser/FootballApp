d87e3572e30f902fd4eae7ae547dffa1f7aea4b6
fatih40 <baserfatih40@gmail.com>
2021-04-08 13:34:51 +0200

    fragments finished

--- app/src/main/java/com/fatihbaser/navigationkotlin/SecondFragment.kt
+++ app/src/main/java/com/fatihbaser/navigationkotlin/SecondFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment() {
@@ -21,6 +24,21 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {// butun goruntuler cagirildiktan sonra aaciliyor
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val kullanici_adi=SecondFragmentArgs.fromBundle(it).usermane
+            println(kullanici_adi)
+            secondFragmentTextview.text=kullanici_adi
+        }
+        button2.setOnClickListener{
+            val action=SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+
     }
 
 
